1975b6824cf6cd650a0c0351776c3e5ed3a019ca
Thais Silva <thais_ap@live.com>
2022-08-31 22:40:05 -0300

    mobile first done

---
+++ script.js
@@ -0,0 +1,16 @@
+let button = document.querySelectorAll('.share__button')
+let about = document.querySelector('.article__about')
+let shareBox = document.querySelector('.share__box')
+
+for (let i = 0; i < button.length; i++) {
+    button[i].addEventListener('click', share)
+}
+
+function share(){
+    if (window.innerWidth <= 768) {
+        about.style.display = 'none'
+        shareBox.style.display = 'flex'
+    } else {
+        console.log("nao deu certo")
+    }
+}
